2527ecc42cdaa1e9b0c77b2b3384750b50d64f5b
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-10-02 00:17:18 -0400

    Fix task sequence dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var uglify = require('gulp-uglify');
 var rm = require('del');
 
 gulp.task('lint', function () {
-	gulp.src('src/*.js')
+	return gulp.src('src/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter())
 		.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('uglify', ['lint'], function () {
-	gulp.src('src/*.js')
+	return gulp.src('src/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist/'));
 });
