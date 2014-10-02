8d027899263bec2ac4e7b867a671749c184aa49d
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-10-02 00:11:56 -0400

    Add gulp clean task. Fix paths and task dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var rm = require('del');
 
 gulp.task('default', function () {
 	console.log("Default task.");
 });
 
 gulp.task('lint', function () {
-	gulp.src('./src/*.js')
+	gulp.src('src/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter())
 		.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('uglify', function () {
-	gulp.src('./src/*.js')
+gulp.task('uglify', ['lint'], function () {
+	gulp.src('src/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('clean', function (callback) {
+	rm(['dist'], callback);
+});
+
+gulp.task('default', ['uglify']);
